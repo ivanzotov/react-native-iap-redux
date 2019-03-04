d931856b3dd0edb66a33c6fa5f6f085bbd750615
Ivan Zotov <ivan_zotov@railsmob.com>
2019-03-04 11:19:12 +0300

    Add eslint

--- src/Purchase.js
+++ src/Purchase.js
@@ -1,59 +1,9 @@
-import { AsyncStorage } from 'react-native';
+import { Alert } from 'react-native';
 import * as RNIap from 'react-native-iap';
+import PurchasesCollection from './PurchasesCollection';
 
 export class Purchase {
-  static CONFIG = {
-    async_storage_key: 'purchases',
-    redux_reducer_key: 'purchases',
-    redux_action_type_load: 'LOAD_PURCHASES',
-    redux_action_type_buy: 'BUY_PURCHASE',
-  };
-
-  static reducer = (state = [], action) => {
-    switch (action.type) {
-      case Purchase.CONFIG.redux_action_type_load:
-        return action.payload;
-      case Purchase.CONFIG.redux_action_type_buy:
-        return [...state, action.sku];
-      default:
-        return state;
-    }
-  };
-
-  static loadFromAsyncStorageToReduxStore = async reduxStore => {
-    const json = await AsyncStorage.getItem(Purchase.CONFIG.async_storage_key);
-    const purchases = JSON.parse(json);
-
-    reduxStore.dispatch({
-      type: Product.CONFIG.redux_action_type_load,
-      payload: ProductCollection.deserialize(products || []),
-    });
-  };
-
-  static deserialize = raw => new Product(raw);
-
-  static restoreProducts = async () => {
-    await RNIap.initConnection();
-
-    try {
-      const availablePurchases = await RNIap.getAvailablePurchases();
-      const products = availablePurchases.map(it => new Purchase(it.productId));
-      const collection = new ProductCollection(products);
-
-      store.dispatch({
-        type: Purchase.CONFIG.redux_action_type_load,
-        payload: collection,
-      });
-
-      const raw = collection.serialize();
-      const json = JSON.stringify(raw);
-      await AsyncStorage.setItem(Product.ASYNC_STORE_KEY, json);
-    } catch (err) {
-      Alert.alert(`Ошибка: ${err.code}`, err.message);
-    }
-
-    await RNIap.endConnection();
-  };
+  static deserialize = raw => new Purchase(raw);
 
   constructor(sku) {
     this.sku = sku;
@@ -76,10 +26,6 @@ export class Purchase {
 
     await RNIap.endConnection();
   };
-
-  isAvailable = () =>
-    store.getState()[Purchase.CONFIG.redux_reducer_key].indexOf(this.sku) !==
-    -1;
 
   getInfo = async () => {
     const products = await RNIap.getProducts([this.sku]);
--- src/PurchasesCollection.js
+++ src/PurchasesCollection.js
@@ -1,20 +1,93 @@
-import Purchase from './index';
+import { AsyncStorage, Alert } from 'react-native';
+import * as RNIap from 'react-native-iap';
+import Purchase from './Purchase';
 
 export class PurchasesCollection {
+  static DEFAULT_REDUX_STORE = null;
+  static CONFIG = {
+    async_storage_key: 'purchases',
+    redux_reducer_key: 'purchases',
+    redux_action_type_load: 'LOAD_PURCHASES',
+    redux_action_type_buy: 'BUY_PURCHASE',
+  };
+
+  static reducer = (state = [], action) => {
+    switch (action.type) {
+      case PurchasesCollection.CONFIG.redux_action_type_load:
+        return action.payload;
+      case PurchasesCollection.CONFIG.redux_action_type_buy:
+        return [...state, action.sku];
+      default:
+        return state;
+    }
+  };
+
+  static loadFromAsyncStorageToReduxStore = async (
+    reduxStore = PurchasesCollection.DEFAULT_REDUX_STORE
+  ) => {
+    const json = await AsyncStorage.getItem(
+      PurchasesCollection.CONFIG.async_storage_key
+    );
+    const purchases = JSON.parse(json);
+
+    reduxStore.dispatch({
+      type: PurchasesCollection.CONFIG.redux_action_type_load,
+      payload: PurchasesCollection.deserialize(purchases || []),
+    });
+  };
+
+  static restorePurchases = async (
+    reduxStore = PurchasesCollection.DEFAULT_REDUX_STORE
+  ) => {
+    await RNIap.initConnection();
+
+    try {
+      const availablePurchases = await RNIap.getAvailablePurchases();
+      const purchases = availablePurchases.map(
+        it => new Purchase(it.productId)
+      );
+      const collection = new PurchasesCollection(purchases);
+
+      reduxStore.dispatch({
+        type: PurchasesCollection.CONFIG.redux_action_type_load,
+        payload: collection,
+      });
+
+      const raw = collection.serialize();
+      const json = JSON.stringify(raw);
+      await AsyncStorage.setItem(
+        PurchasesCollection.CONFIG.async_storage_key,
+        json
+      );
+    } catch (err) {
+      Alert.alert(`Ошибка: ${err.code}`, err.message);
+    }
+
+    await RNIap.endConnection();
+  };
+
   static deserialize = raw => {
     const purchases = raw.map(it => Purchase.deserialize(it));
     return new PurchasesCollection(purchases);
   };
 
-  constructor(purchase) {
+  static fromReduxStore = (
+    reduxStore = PurchasesCollection.DEFAULT_REDUX_STORE
+  ) => reduxStore.getState()[PurchasesCollection.CONFIG.redux_reducer_key];
+
+  static isAvailable = (
+    purchase,
+    reduxStore = PurchasesCollection.DEFAULT_REDUX_STORE
+  ) => {
+    const collection = PurchasesCollection.fromReduxStore(reduxStore);
+    return collection.getPurchases().indexOf(purchase) !== -1;
+  };
+
+  constructor(purchases) {
     this.__purchases = purchases;
   }
 
-  getPurchases() {
-    return this.__purchases;
-  }
+  getPurchases = () => this.__purchases;
 
-  serialize = () => {
-    return this.getPurchases().map(it => it.serialize());
-  };
+  serialize = () => this.getPurchases().map(it => it.serialize());
 }
